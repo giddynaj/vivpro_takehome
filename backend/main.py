2b9063026f2271a97b7eecee087656bf795e124b
giddynaj <eberjan@gmail.com>
2026-01-07 12:16:33 -0500

    Adds CORS for future frontend access

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+
+origins = [
+    'http://localhost:5173'
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+df = pd.read_json('./playlist.json')
 
 @app.get("/")
 async def initialize():
-    print("Initialized")
+    print(df.transpose())
+    return { "status": "ok"}
